bc9b17d2ef96b9ad407eafed5fcbb137e5da8386
Taru <tarudima@rambler.ru>
2022-04-24 17:14:01 +0300

    latin words check

--- lesson03/tast06.py
+++ lesson03/tast06.py
@@ -4,16 +4,18 @@
 # разделенных пробелом. Каждое слово состоит из латинских букв в нижнем регистре.
 # Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 # Необходимо использовать написанную ранее функцию int_func().
+import re
+
 
 def int_func(words):
     result = ""
-    if words == words.lower():
+    if words == words.lower() and re.search(r'[^a-z]', words) == None:
         words = words.split(" ")
         for pos in range(len(words)):
             result += words[pos].lower().capitalize() + " "
         return result.rstrip(" ")
     else:
-        return "Ошибка! Строка должна быть из слов в нижнем регистре"
+        return "Ошибка! Строка должна быть из слов на латинице и в нижнем регистре"
 
 words = input("Введите строку из слов, разделенных пробелом: ")
 
